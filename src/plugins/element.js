dff74a48e0acc1a8d6bb17fc581cd3a78dc8529f
孔德锦 <7927567+kong_dejin@user.noreply.gitee.com>
2020-10-29 17:07:28 +0800

    完成了登录功能

---
+++ src/plugins/element.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// 需要全局挂载信息弹框message
+Vue.prototype.$message = Message
